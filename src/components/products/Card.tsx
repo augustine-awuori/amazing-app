c2737ff2266d8e2dd0b0f2d4611e360f355caec4
augustine-awuori <codewithaugustine@gmail.com>
2024-05-30 23:08:59 +0300

    Improve the look and feel

--- src/components/products/Card.tsx
+++ src/components/products/Card.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { Product } from "../../hooks/useProducts";
@@ -5,31 +6,39 @@ import { funcs } from "../../utils";
 import ShoppingCartIcon from "../ShoppingCartIcon";
 
 const ProductCard = ({ _id, name, description, price, images }: Product) => {
+  const [hovered, setHovered] = useState(false);
   const navigate = useNavigate();
 
+  const handleMouseEnter = () => setHovered(true);
+  const handleMouseLeave = () => setHovered(false);
+
   return (
-    <div
-      className="w-full p-2 cursor-pointer"
+    <article
+      className="w-full p-1 cursor-pointer relative overflow-hidden"
       onClick={() => navigate(`/products/${_id}`)}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
-      <div className="card bg-base-100 shadow-xl">
-        <figure>
-          <img
-            src={images[0]}
-            alt={name}
-            className="w-full h-48 object-cover"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{name}</h2>
-          <p className="line-clamp-2">{description}</p>
-          <button className="btn btn-primary btn-block mt-2">
+      <article className="card bg-base-100 shadow-xl relative">
+        <img
+          src={images[0]}
+          alt={name}
+          className="w-full h-48 object-cover rounded-lg"
+        />
+        <article className="rounded-b-md absolute bottom-0 left-0 right-0 py-2 px-3 bg-black bg-opacity-50 text-white">
+          {hovered && (
+            <article style={{ transition: "opacity 0.3s ease-in-out" }}>
+              <h2 className="card-title text-lg">{name}</h2>
+              <p className="line-clamp-2">{description}</p>
+            </article>
+          )}
+          <button className="btn btn-primary btn-block">
             <ShoppingCartIcon />
             Ksh {funcs.addComma(price)}
           </button>
-        </div>
-      </div>
-    </div>
+        </article>
+      </article>
+    </article>
   );
 };
 
--- src/components/products/Grid.tsx
+++ src/components/products/Grid.tsx
@@ -9,7 +9,7 @@ const ProductsGrid = ({ products }: Props) => {
   return (
     <section className="p-4">
       <article className="flex justify-center">
-        <article className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+        <article className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-4 w-full">
           {products.map((p) => (
             <ProductCard key={p._id} {...p} />
           ))}
