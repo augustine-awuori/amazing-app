37a47bde79c7921e1284f823423a16d96054db10
augustine-awuori <codewithaugustine@gmail.com>
2024-06-08 12:15:47 +0300

    Show loading skeletons when fetching products for the first time

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -37,6 +37,7 @@ export default () => {
   }, []);
 
   const prepareProducts = async () => {
+    if (!products.length) setLoading(true);
     const data = await getProducts();
 
     if (!products.length) {
