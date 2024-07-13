7ec70a8627c990a03b7d88df983566e564948242
augustine-awuori <codewithaugustine@gmail.com>
2024-07-13 17:51:57 +0300

    Make buttons function

--- src/pages/NotLoggedInPage.tsx
+++ src/pages/NotLoggedInPage.tsx
@@ -1,8 +1,36 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+
+import { LoginForm, Modal, RegisterForm } from "../components";
 
 const NotLoggedInPage = () => {
+  const [showLoginForm, setShowLoginForm] = useState(false);
+  const [showRegisterForm, setShowRegisterForm] = useState(false);
+
+  const switchToSignUp = () => {
+    setShowLoginForm(false);
+    setShowRegisterForm(true);
+  };
+
+  const switchToSignIn = () => {
+    setShowLoginForm(true);
+    setShowRegisterForm(false);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center">
+      <Modal
+        content={<LoginForm onSignUpRequest={switchToSignUp} />}
+        isOpen={showLoginForm}
+        onClose={() => setShowLoginForm(false)}
+        title=""
+      />
+      <Modal
+        content={<RegisterForm onLoginRequest={switchToSignIn} />}
+        isOpen={showRegisterForm}
+        onClose={() => setShowRegisterForm(false)}
+        title=""
+      />
+
       <div className="max-w-md mx-auto p-8  rounded-lg shadow-lg">
         <h2 className="text-3xl font-semibold text-center mb-4">
           You're not logged in
@@ -11,18 +39,18 @@ const NotLoggedInPage = () => {
           Please log in to access this page.
         </p>
         <div className="flex justify-center space-x-4">
-          <Link
-            to="/login"
+          <p
             className="btn btn-primary py-2 px-4 text-white rounded-lg hover:bg-primary-dark"
+            onClick={() => setShowLoginForm(true)}
           >
             Sign In
-          </Link>
-          <Link
-            to="/signup"
+          </p>
+          <p
             className="btn btn-secondary py-2 px-4 text-white rounded-lg hover:bg-secondary-dark"
+            onClick={() => setShowRegisterForm(true)}
           >
             Sign Up
-          </Link>
+          </p>
         </div>
       </div>
     </div>
