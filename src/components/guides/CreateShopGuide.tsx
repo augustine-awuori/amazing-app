9a532a00656dd88364885d89ac66bec4d2068981
augustine-awuori <codewithaugustine@gmail.com>
2024-07-20 18:14:19 +0300

    Improve the look and feel

--- src/components/guides/CreateShopGuide.tsx
+++ src/components/guides/CreateShopGuide.tsx
@@ -5,7 +5,7 @@ import YouTubeVideo from "./YouTubeVideo";
 
 const CreateShopGuide = () => {
   const navigate = useNavigate();
-  const p = "https://youtu.be/r1VgPILjMjc";
+
   const Link = ({ children, route }: { children: string; route: string }) => (
     <p
       className="inline text-blue-600 cursor-pointer"
@@ -40,40 +40,58 @@ const CreateShopGuide = () => {
         Once you’re logged in, go to the <Link route="/">home page</Link>
       </Text>
       <Subtitle> 4. Create Your Shop</Subtitle>
-      <Text>
-        On mobile devices, click the plus icon (+) located at the top right just
-        below the navigation bar to start the shop creation process. On larger
-        screens, click the 'Add Product' button.
-      </Text>
-      <Text>
-        A 'Choose Shop' modal will appear. If you already have a shop, you can
-        select it from the list. If you don’t have a shop yet, follow these
-        steps to create one:
-      </Text>
-      <Text>
-        Click on{" "}
-        <Link route="/mart/shops/new">'create a new shop instead'</Link>
-        {"  "} at the top of the modal and you'll be directed to a page to input
-        your shop's details
-      </Text>
-      <Text>Choose an image to represent your shop</Text>
-      <Text>
-        Input the name and shop type (e.g., clothing). Your shop can have more
-        than one type, allowing you to sell different items at once.
-      </Text>
-      <Text>
-        State the location of your shop. Potential buyers often make purchasing
-        decisions based on the shop's location.
-      </Text>
-      <Text>
-        Finally, save the information and WAIT for the shop to be created. If
-        you navigate to other pages, you may interfere with the creation
-        process.
-      </Text>
-      <Text>
-        Your shop is now successfully created. You can see it at the{" "}
-        <Link route="/mart/shops">shops' page</Link>{" "}
-      </Text>
+      <ol className="list-disc ml-10">
+        <li>
+          <Text>
+            On mobile devices, click the plus icon (+) located at the top right
+            just below the navigation bar to start the shop creation process. On
+            larger screens, click the 'Add Product' button.
+          </Text>
+        </li>
+        <li>
+          <Text>
+            A 'Choose Shop' modal will appear. If you already have a shop, you
+            can select it from the list. If you don’t have a shop yet, follow
+            these steps to create one:
+          </Text>
+        </li>
+        <li>
+          <Text>
+            Click on{" "}
+            <Link route="/mart/shops/new">'create a new shop instead'</Link>
+            {"  "} at the top of the modal and you'll be directed to a page to
+            input your shop's details
+          </Text>
+        </li>
+        <li>
+          <Text>Choose an image to represent your shop</Text>
+        </li>
+        <li>
+          <Text>
+            Input the name and shop type (e.g., clothing). Your shop can have
+            more than one type, allowing you to sell different items at once.
+          </Text>
+        </li>
+        <li>
+          <Text>
+            State the location of your shop. Potential buyers often make
+            purchasing decisions based on the shop's location.
+          </Text>
+        </li>
+        <li>
+          <Text>
+            Finally, save the information and WAIT for the shop to be created.
+            If you navigate to other pages, you may interfere with the creation
+            process.
+          </Text>
+        </li>
+        <li>
+          <Text>
+            Your shop is now successfully created. You can see it at the{" "}
+            <Link route="/mart/shops">shops' page</Link>{" "}
+          </Text>
+        </li>
+      </ol>
 
       <Subtitle className="mb-3">Watch Video Instead</Subtitle>
       <YouTubeVideo src="https://youtu.be/ZMD-x5Ox1s8" />
